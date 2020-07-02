7a7bd2b0b2293f298904237f95366b7b137e9c95
Oleksii Mukhin <oleksii.mukhin@gmail.com>
2020-07-02 13:43:37 +0300

    make only column name unique

--- db/migrations/20200701151350_create_project_table.up.sql
+++ db/migrations/20200701151350_create_project_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS project(
    id serial PRIMARY KEY,
-   name VARCHAR (500) UNIQUE NOT NULL,
+   name VARCHAR (500) NOT NULL,
    description TEXT
 );
--- db/migrations/20200701151443_create_task_table.up.sql
+++ db/migrations/20200701151443_create_task_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS task(
    id serial PRIMARY KEY,
-   name VARCHAR (500) UNIQUE NOT NULL,
+   name VARCHAR (500) NOT NULL,
    description TEXT,
    position INTEGER NOT NULL,
    column_id INTEGER NOT NULL,
