8b83dab545e080131683ff344f51713b151bf062
Oleksii Mukhin <oleksii.mukhin@gmail.com>
2020-07-03 10:01:43 +0300

    add env var

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"go.uber.org/zap"
 	"log"
 	"net/http"
+	"os"
 	"task-manager/adapters"
 	"task-manager/handlers"
 )
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
-	logger := zap.NewExample()
+	logger, _ := zap.NewProduction()
+	mode, exists := os.LookupEnv("MODE")
+	if exists && mode == "development" {
+		logger, _ = zap.NewDevelopment()
+	}
 	defer func() {
 		if err := logger.Sync(); err != nil {
 			logger.Error(err.Error())
@@ -21,7 +34,27 @@ func main() {
 	undo := zap.ReplaceGlobals(logger)
 	defer undo()
 
-	connStr := "user=postgres password=manager dbname=task_manager host=localhost sslmode=disable"
+	postgresUser, exist := os.LookupEnv("POSTGRES_USER")
+	if !exist {
+		postgresUser = "postgres"
+	}
+	postgresPassword, exist := os.LookupEnv("POSTGRES_PASSWORD")
+	if !exist {
+		postgresPassword = "manager"
+	}
+	postgresDb, exist := os.LookupEnv("POSTGRES_DB")
+	if !exist {
+		postgresDb = "task_manager"
+	}
+	postgresHost, exist := os.LookupEnv("POSTGRES_HOST")
+	if !exist {
+		postgresHost = "localhost"
+	}
+
+	connStr := fmt.Sprintf(
+		"user=%s password=%s dbname=%s host=%s sslmode=disable",
+		postgresUser, postgresPassword, postgresDb, postgresHost,
+	)
 	conn, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
